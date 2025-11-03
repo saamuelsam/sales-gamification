be7feb82402429786ab0de503c8bfc6dbf984b52
Samuel Anselmo <samuelanselmo69@gmail.com>
2025-11-03 13:41:15 -0300

    dash

--- frontend/src/features/dashboard/hooks/useDashboard.ts
+++ frontend/src/features/dashboard/hooks/useDashboard.ts
@@ -1,7 +1,6 @@
 // frontend/src/features/dashboard/hooks/useDashboard.ts
 import { useQuery } from '@tanstack/react-query';
-import api from '@/services/api';
-
+import api  from '@/services/api';
 
 export interface DashboardPersonal {
   sales: {
